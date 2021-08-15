45e2fa7c8d70e3eaa68e6e5c249d4fde799f50db
kairusds <harveyhansjavier@gmail.com>
2021-08-16 04:06:05 +0800

    try putting group checkable before inflate

--- app/src/main/java/github/kairusds/android30/MainActivity.java
+++ app/src/main/java/github/kairusds/android30/MainActivity.java
@@ -146,8 +146,8 @@ public class MainActivity extends AppCompatActivity{
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu){
-		getMenuInflater().inflate(R.menu.menu_main, menu);
 		menu.setGroupCheckable(R.id.settings_items, true, false);
+		getMenuInflater().inflate(R.menu.menu_main, menu);
 		return true;
 	}
 
