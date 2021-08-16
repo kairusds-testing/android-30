b1f538a58baa51df1ae5aaad8634fb057e039444
kairusds <harveyhansjavier@gmail.com>
2021-08-17 02:52:52 +0800

    Don't use Sticky Immersive

--- app/src/main/java/github/kairusds/android30/MainActivity.java
+++ app/src/main/java/github/kairusds/android30/MainActivity.java
@@ -167,7 +167,7 @@ public class MainActivity extends AppCompatActivity{
 	private void hideSystemUI() {
 		View decorView = getWindow().getDecorView();
 		decorView.setSystemUiVisibility(
-			View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+			View.SYSTEM_UI_FLAG_IMMERSIVE
 			| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 			| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 			| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
